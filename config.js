d33536b41e3fd41f88992f7996f950960c984213
Shweta Kuthi <shweta.kuthi@nagarro.com>
2023-06-22 17:57:12 +0530

    changednodeimage tag

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
    db : {
-        host : env.host || 'localhost',
-        port : env.port || '3306',
-        password : env.password || 'root',
-        user : env.user || 'root',
-        database : env.database || 'user_db',
+        host : env.DB_HOST || 'localhost',
+        port : env.DB_PORT || '3306',
+        password : env.DB_PASSWORD || 'root',
+        user : env.DB_USER || 'root',
+        database : env.DB_NAME || 'user_db',
         debug : env.debug || false,
    },
    listPerPage: env.LIST_PER_PAGE || 10,
