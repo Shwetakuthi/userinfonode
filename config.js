a0880f1fd0776fed63cac37a9c828071cbfaaa76
Shweta Kuthi <shweta.kuthi@nagarro.com>
2023-06-23 12:52:13 +0530

    changednodeimage tag

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const env = process.env;
 
 const config = {
    db : {
-        host : env.DB_HOST || 'localhost',
+        host : 'localhost',
         port : env.DB_PORT || '3306',
         password : env.DB_PASSWORD || 'root',
         user : env.DB_USER || 'root',
--- index.js
+++ index.js
@@ -30,53 +30,53 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 //end body-parser configuration
 
 //create app server
-var server = app.listen(3010, function () {
+// var server = app.listen(3010, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+//   var host = server.address().address
+//   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+//   console.log("Example app listening at http://%s:%s", host, port)
 
-});
+// });
 
-//rest api to get all customers
-app.get('/users', function (req, res) {
-   connection.query('select * from user', function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
-});
-//rest api to get a single customer data
-app.get('/users/:id', function (req, res) {
-   connection.query('select * from user where user_id=?', [req.params.id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
-});
+// //rest api to get all customers
+// app.get('/users', function (req, res) {
+//    connection.query('select * from user', function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  res.end(JSON.stringify(results));
+// 	});
+// });
+// //rest api to get a single customer data
+// app.get('/users/:id', function (req, res) {
+//    connection.query('select * from user where user_id=?', [req.params.id], function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  res.end(JSON.stringify(results));
+// 	});
+// });
 
-//rest api to create a new customer record into mysql database
-app.post('/users', function (req, res) {
-   var params  = req.body;
-   console.log(params);
-   connection.query('INSERT INTO user SET ?', params, function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
-});
+// //rest api to create a new customer record into mysql database
+// app.post('/users', function (req, res) {
+//    var params  = req.body;
+//    console.log(params);
+//    connection.query('INSERT INTO user SET ?', params, function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  res.end(JSON.stringify(results));
+// 	});
+// });
 
-//rest api to update record into mysql database
-app.put('/users', function (req, res) {
-   connection.query('UPDATE `customer` SET `Name`=?,`Address`=?,`Country`=?,`Phone`=? where `Id`=?', [req.body.Name,req.body.Address, req.body.Country, req.body.Phone, req.body.Id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
-});
+// //rest api to update record into mysql database
+// app.put('/users', function (req, res) {
+//    connection.query('UPDATE `customer` SET `Name`=?,`Address`=?,`Country`=?,`Phone`=? where `Id`=?', [req.body.Name,req.body.Address, req.body.Country, req.body.Phone, req.body.Id], function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  res.end(JSON.stringify(results));
+// 	});
+// });
 
-//rest api to delete record from mysql database
-app.delete('/users', function (req, res) {
-   console.log(req.body);
-   connection.query('DELETE FROM `user` WHERE `user_id`=?', [req.body.Id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end('Record has been deleted!');
-	});
-});
+// //rest api to delete record from mysql database
+// app.delete('/users', function (req, res) {
+//    console.log(req.body);
+//    connection.query('DELETE FROM `user` WHERE `user_id`=?', [req.body.Id], function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  res.end('Record has been deleted!');
+// 	});
+// });
